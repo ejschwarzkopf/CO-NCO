82d47743643977d1d340d6a53b979442368e30e4
Enrique Schwarzkopf (BCSC - Caiti Heil) <eschwar3@sisko.statgen.ncsu.edu>
2024-03-26 14:20:19 -0400

    Not sure. Just kind of played around with stuff without updating the github

--- downsample/downsample_averages.sh
+++ downsample/downsample_averages.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 #SBATCH -J Downsample
-#SBATCH --error=/home4/eschwar3/CO_NCO/1.scripts/1.logs/Downsample_%a.err
-#SBATCH --output=/home4/eschwar3/CO_NCO/1.scripts/1.logs/Downsample_%a.out
+#SBATCH --error=/home4/eschwar3/CO_NCO/1.scripts/1.logs/Downsample_avg.err
+#SBATCH --output=/home4/eschwar3/CO_NCO/1.scripts/1.logs/Downsample_avg.out
 
 module load R/4.1.0
 
 
-cd ~/CO_NCO/3.output/2.CrossOver/SRR1119200XSRR1119199_Unmasked/subsampling/subsample_$k/tables
+cd ~/CO_NCO/3.output/2.CrossOver/SRR1119200XSRR1119199_Unmasked/subsampling
+
+for i in 1 1.58 2 4 8 11.9 16 32 40; do cat subsample_$i/tables/CO_NCO_table_* > subsample_$i/tables/CO_NCO_${i}_table.txt; done
+	
+for i in 1 1.58 2 4 8 11.9 16 32 40; do for j in `seq 1 20`; do echo $i; done > tmp.txt; paste tmp.txt subsample_$i/tables/CO_NCO_${i}_table.txt; done > CO_NCO_table_all.txt
 
 Rscript -e 'CO_NCO_list<-list()
 CO_NCO_table<-data.frame(CO_mean=c(), CO_se=c(), NCO_mean=c(), CO_mean=c())
 std.err <- function(x) sd(x)/sqrt(length(x))
-dens<-c(1,1.58,2,4,8,16,32,40)
-for(k in 1:8){
-filename<-paste0("subsample_", dens[k], "_table.txt")
+dens<-c(1,1.58,2,4,8,11.9,16,32,40)
+for(k in 1:9){
+filename<-paste0("subsample_", dens[k], "/tables/CO_NCO_", dens[k], "_table.txt")
 CO_NCO_list[[k]]<-read.table(filename)
-CO_NCO_table[k,]<-data.frame(CO_mean=mean(CO_NCO_list[[k]][,1]), CO_se=ste.err(CO_NCO_list[[k]][,1]), NCO_mean=mean(CO_NCO_list[[k]][,3]), CO_mean=std.err(CO_NCO_list[[k]][,3])
+CO_NCO_table<-rbind(CO_NCO_table,data.frame(CO_mean=mean(CO_NCO_list[[k]][,1]), CO_se=std.err(CO_NCO_list[[k]][,1]), NCO_mean=mean(CO_NCO_list[[k]][,3]), NCO_se=std.err(CO_NCO_list[[k]][,3])))
 }
 rownames(CO_NCO_table)=dens
-write.table(CO_NCO_table, "CO_NCO_table.txt, quote=F")'
+write.table(CO_NCO_table, "CO_NCO_table.txt", quote=F)'
 
+sed -i '1s/^/Density /' CO_NCO_table.txt
 
 
--- downsample/downsample_crossover.sh
+++ downsample/downsample_crossover.sh
@@ -2,7 +2,7 @@
 #SBATCH -J Downsample
 #SBATCH --error=/home4/eschwar3/CO_NCO/1.scripts/1.logs/Downsample_%a.err
 #SBATCH --output=/home4/eschwar3/CO_NCO/1.scripts/1.logs/Downsample_%a.out
-#SBATCH --array=1-180
+#SBATCH --array=60-93
 
 module load python/2.7.18
 module load R/4.1.0
@@ -13,20 +13,21 @@ k=$SLURM_ARRAY_TASK_ID
 
 i=$(awk 'NR=='$k' {print $1}' downsample_aux.txt)
 j=$(awk 'NR=='$k' {print $2}' downsample_aux.txt)
+seed=$(awk 'NR=='$k' {print $3}' downsample_aux.txt)
 
 Rscript -e 'allSNPs<-read.table("subsample_'$i'/replicate_'$j'/downsample_SNPs.txt")
 i='$i'
-set.seed('$k')
+set.seed('$seed')
 if(i==1){l=194}else if(i==1.58){l=307}else if(i==2){l=388}else if(i==4){l=776}else if(i==8){l=1552}else if(i==11.9){l=2309}else if(i==16){l=3104}else if(i==32){l=6208}else if(i==40){l=7760}else{NULL}
-sample<-sort(floor(runif(n=l, min=1, max=10609)))
-sample_list=allSNPs[sample,]
+samp=sample(1:10609, l, F)
+sample_list=allSNPs[samp,]
 # This is where I need to load all of the seg files, extract the sampled snps and write the downsampled seg files
 segfiles<-list()
 for(k in c(1:26, 28:48)){
 filename1=paste0("~/CO_NCO/3.output/2.CrossOver/SRR1119200XSRR1119199_Unmasked/subsampling/segfiles/SRR1119200XSRR1119199_", k, ".txt")
 segfile<-read.table(filename1, header=F)
 segfiles[[k]]<-segfile
-segfiles[[k]]=rbind(segfiles[[k]][which(segfiles[[k]][,1]<4),], segfiles[[k]][which(segfiles[[k]][,1]==4 & segfiles[[k]][,2]>236000),], segfiles[[k]][which(segfiles[[k]][,1] == 4 & segfiles[[k]][,2] %in% sample_list[,2]),], segfiles[[k]][which(segfiles[[k]][,1]==4 & segfiles[[k]][,3]<430000),], segfiles[[k]][which(segfiles[[k]][,1]>4),])
+segfiles[[k]]=rbind(segfiles[[k]][which(segfiles[[k]][,1]<14),], segfiles[[k]][which(segfiles[[k]][,1]==14 & segfiles[[k]][,2]<236000),], segfiles[[k]][which(segfiles[[k]][,1] == 14 & segfiles[[k]][,2] %in% sample_list[,2]),], segfiles[[k]][which(segfiles[[k]][,1]==14 & segfiles[[k]][,2]>430000),], segfiles[[k]][which(segfiles[[k]][,1]>14),])
 filename=paste0("subsample_'$i'/replicate_'$j'/CrossOver_v6.3/segfiles/SRR1119200XSRR1119199_", k, ".txt")
 write.table(segfiles[[k]], filename, quote=F, col.names=F, row.names=F)
 }
@@ -88,22 +89,22 @@ NCOs1_13<-NCOs1[which(NCOs1$chr==13),]
 NCOs1_14<-NCOs1[which(NCOs1$chr==14),]
 NCOs1_15<-NCOs1[which(NCOs1$chr==15),]
 NCOs1_16<-NCOs1[which(NCOs1$chr==16),]
-COs1_count_1=data.frame(chr=1,start=10000*((1:(Suva_chrom_length[1,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[1,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[1,1]/10000+1))-1)+5000.5)
-COs1_count_2=data.frame(chr=2,start=10000*((1:(Suva_chrom_length[2,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[2,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[2,1]/10000+1))-1)+5000.5)
-COs1_count_3=data.frame(chr=3,start=10000*((1:(Suva_chrom_length[3,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[3,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[3,1]/10000+1))-1)+5000.5)
-COs1_count_4=data.frame(chr=4,start=10000*((1:(Suva_chrom_length[4,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[4,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[4,1]/10000+1))-1)+5000.5)
-COs1_count_5=data.frame(chr=5,start=10000*((1:(Suva_chrom_length[5,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[5,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[5,1]/10000+1))-1)+5000.5)
-COs1_count_6=data.frame(chr=6,start=10000*((1:(Suva_chrom_length[6,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[6,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[6,1]/10000+1))-1)+5000.5)
-COs1_count_7=data.frame(chr=7,start=10000*((1:(Suva_chrom_length[7,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[7,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[7,1]/10000+1))-1)+5000.5)
-COs1_count_8=data.frame(chr=8,start=10000*((1:(Suva_chrom_length[8,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[8,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[8,1]/10000+1))-1)+5000.5)
-COs1_count_9=data.frame(chr=9,start=10000*((1:(Suva_chrom_length[9,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[9,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[9,1]/10000+1))-1)+5000.5)
-COs1_count_10=data.frame(chr=10,start=10000*((1:(Suva_chrom_length[10,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[10,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[10,1]/10000+1))-1)+5000.5)
-COs1_count_11=data.frame(chr=11,start=10000*((1:(Suva_chrom_length[11,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[11,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[11,1]/10000+1))-1)+5000.5)
-COs1_count_12=data.frame(chr=12,start=10000*((1:(Suva_chrom_length[12,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[12,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[12,1]/10000+1))-1)+5000.5)
-COs1_count_13=data.frame(chr=13,start=10000*((1:(Suva_chrom_length[13,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[13,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[13,1]/10000+1))-1)+5000.5)
-COs1_count_14=data.frame(chr=14,start=10000*((1:(Suva_chrom_length[14,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[14,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[14,1]/10000+1))-1)+5000.5)
-COs1_count_15=data.frame(chr=15,start=10000*((1:(Suva_chrom_length[15,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[15,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[15,1]/10000+1))-1)+5000.5)
-COs1_count_16=data.frame(chr=16,start=10000*((1:(Suva_chrom_length[16,1]/10000+1))-1)+1,end=10000*((1:(Suva_chrom_length[16,1]/10000+1))),mid=10000*((1:(Suva_chrom_length[16,1]/10000+1))-1)+5000.5)
+COs1_count_1=data.frame(chr=1,start=20000*((1:(Suva_chrom_length[1,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[1,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[1,1]/20000+1))-1)+10000.5)
+COs1_count_2=data.frame(chr=2,start=20000*((1:(Suva_chrom_length[2,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[2,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[2,1]/20000+1))-1)+10000.5)
+COs1_count_3=data.frame(chr=3,start=20000*((1:(Suva_chrom_length[3,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[3,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[3,1]/20000+1))-1)+10000.5)
+COs1_count_4=data.frame(chr=4,start=20000*((1:(Suva_chrom_length[4,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[4,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[4,1]/20000+1))-1)+10000.5)
+COs1_count_5=data.frame(chr=5,start=20000*((1:(Suva_chrom_length[5,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[5,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[5,1]/20000+1))-1)+10000.5)
+COs1_count_6=data.frame(chr=6,start=20000*((1:(Suva_chrom_length[6,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[6,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[6,1]/20000+1))-1)+10000.5)
+COs1_count_7=data.frame(chr=7,start=20000*((1:(Suva_chrom_length[7,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[7,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[7,1]/20000+1))-1)+10000.5)
+COs1_count_8=data.frame(chr=8,start=20000*((1:(Suva_chrom_length[8,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[8,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[8,1]/20000+1))-1)+10000.5)
+COs1_count_9=data.frame(chr=9,start=20000*((1:(Suva_chrom_length[9,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[9,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[9,1]/20000+1))-1)+10000.5)
+COs1_count_10=data.frame(chr=10,start=20000*((1:(Suva_chrom_length[10,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[10,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[10,1]/20000+1))-1)+10000.5)
+COs1_count_11=data.frame(chr=11,start=20000*((1:(Suva_chrom_length[11,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[11,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[11,1]/20000+1))-1)+10000.5)
+COs1_count_12=data.frame(chr=12,start=20000*((1:(Suva_chrom_length[12,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[12,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[12,1]/20000+1))-1)+10000.5)
+COs1_count_13=data.frame(chr=13,start=20000*((1:(Suva_chrom_length[13,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[13,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[13,1]/20000+1))-1)+10000.5)
+COs1_count_14=data.frame(chr=14,start=20000*((1:(Suva_chrom_length[14,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[14,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[14,1]/20000+1))-1)+10000.5)
+COs1_count_15=data.frame(chr=15,start=20000*((1:(Suva_chrom_length[15,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[15,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[15,1]/20000+1))-1)+10000.5)
+COs1_count_16=data.frame(chr=16,start=20000*((1:(Suva_chrom_length[16,1]/20000+1))-1)+1,end=20000*((1:(Suva_chrom_length[16,1]/20000+1))),mid=20000*((1:(Suva_chrom_length[16,1]/20000+1))-1)+10000.5)
 COs1_count_1$COcount=apply(COs1_count_1,1,function(x){length(which(COs1_1$pos.bp.>=x[2]&COs1_1$pos.bp.<=x[3]))})
 COs1_count_2$COcount=apply(COs1_count_2,1,function(x){length(which(COs1_2$pos.bp.>=x[2]&COs1_2$pos.bp.<=x[3]))})
 COs1_count_3$COcount=apply(COs1_count_3,1,function(x){length(which(COs1_3$pos.bp.>=x[2]&COs1_3$pos.bp.<=x[3]))})
@@ -137,7 +138,7 @@ COs1_count_14$NCOcount=apply(COs1_count_14,1,function(x){length(which(NCOs1_14$p
 COs1_count_15$NCOcount=apply(COs1_count_15,1,function(x){length(which(NCOs1_15$pos.bp.>=x[2]&NCOs1_15$pos.bp.<=x[3]&NCOs1_15$tracttype!=1&NCOs1_15$tracttype!=10))})
 COs1_count_16$NCOcount=apply(COs1_count_16,1,function(x){length(which(NCOs1_16$pos.bp.>=x[2]&NCOs1_16$pos.bp.<=x[3]&NCOs1_16$tracttype!=1&NCOs1_16$tracttype!=10))})
 COs1_count_all=rbind(COs1_count_1,COs1_count_2,COs1_count_3,COs1_count_4,COs1_count_5,COs1_count_6,COs1_count_7,COs1_count_8,COs1_count_9,COs1_count_10,COs1_count_11,COs1_count_12,COs1_count_13,COs1_count_14,COs1_count_15,COs1_count_16)
-windows=which(COs1_count_all$chr==4&COs1_count_all$start>=236000&COs1_count_all$end<=430000)
+windows=which(COs1_count_all$chr==14&COs1_count_all$start>=236000&COs1_count_all$end<=430000)
 d=COs1_count_all[windows,]
 std.err <- function(x) sd(x)/sqrt(length(x))
 Count_output=data.frame(CO_mean=mean(d$COcount),CO_se=std.err(d$COcount),NCO_mean=mean(d$NCOcount),NCO_se=std.err(d$NCOcount))
@@ -145,7 +146,6 @@ write.table(Count_output,"CO_NCO_table.txt",quote=F,row.names=F,col.names=F)'
 
 mkdir ~/CO_NCO/3.output/2.CrossOver/SRR1119200XSRR1119199_Unmasked/subsampling/subsample_$i/tables
 mv CO_NCO_table.txt ~/CO_NCO/3.output/2.CrossOver/SRR1119200XSRR1119199_Unmasked/subsampling/subsample_$i/tables/CO_NCO_table_$j.txt
-
 
 
 
